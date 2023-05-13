fe25fd69d13542d2eb5240c7aa457d48958280aa
FancyFelicious <felix.piber@gmail.com>
2023-05-13 14:15:54 +0200

    init :)

---
+++ src/test/at/technikum/tourplanner/HelloApplicationTest.java
@@ -0,0 +1,14 @@
+package at.technikum.tourplanner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloApplicationTest {
+
+    @Test
+    void ok123() {
+        assertEquals(1, 1);
+    }
+
+}
