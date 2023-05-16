49d57b4482b358013f21a5a5bc2785c5823f2763
FancyFelicious <felix.piber@gmail.com>
2023-05-16 02:02:42 +0200

    clean up module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,11 +9,6 @@ module org.fancylynx {
 
     opens org.fancylynx to javafx.fxml;
     opens org.fancylynx.playground to com.fasterxml.jackson.databind;
-    opens org.fancylynx.application.config to javafx.fxml;
 
     exports org.fancylynx;
-//    exports org.fancylynx.application.config;
-//    exports org.fancylynx.playground;
-//    exports org.fancylynx.http;
-//    exports org.fancylynx.application.database;
 }
