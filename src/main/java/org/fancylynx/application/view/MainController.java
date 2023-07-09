c051b907af96a78ba780fb79412555d1fc45ea7a
L-ynx <raza.gh97@gmail.com>
2023-07-10 00:54:28 +0200

    add main window details

--- src/main/java/org/fancylynx/application/view/MainController.java
+++ src/main/java/org/fancylynx/application/view/MainController.java
@@ -1,5 +1,10 @@
 package org.fancylynx.application.view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import org.fancylynx.application.viewmodel.HomeViewModel;
 import org.fancylynx.application.viewmodel.MainViewModel;
 
@@ -7,12 +12,23 @@ import java.io.IOException;
 
 public class MainController {
 
+    @FXML
+    private TabPane tabPane;
+    @FXML
+    private Tab contentTab;
     private ViewHandler viewHandler;
     private MainViewModel viewModel;
 
-    public void init(MainViewModel mainViewModel, ViewHandler viewHandler) {
+    public void init(MainViewModel mainViewModel, ViewHandler viewHandler) throws IOException {
         this.viewModel = mainViewModel;
         this.viewHandler = viewHandler;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("TourDetails.fxml"));
+        AnchorPane content = loader.load();
+        contentTab.setContent(content);
+
+        AnchorPane detailsAnchor = (AnchorPane) content.lookup("#detailsAnchor");
+        detailsAnchor.prefWidthProperty().bind(tabPane.widthProperty());
+        detailsAnchor.prefHeightProperty().bind(tabPane.heightProperty());
     }
 
     public void handleCreateTourButton() throws IOException {
